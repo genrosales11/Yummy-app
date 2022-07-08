359b5bb89cd3db5bfa984cebfbfa258f1ebcbbcc
laurasierra17 <laura.sierra17@gmail.com>
2022-07-08 13:41:39 -0400

    add dummy markers to the map

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,33 @@
 // Initialize Google Maps API
-// var mapContainer = $("#map");
 var map;
+// Dummy data to display restaurants in a given area
+var restaurants = [
+    {
+        name: "Hawkers Asian Street Food",
+        lat: 28.560418,
+        lng: -81.364372
+    },
+    {
+        name: "Izziban Sushi & BBQ",
+        lat: 28.5535468,
+        lng: -81.3222981
+    }
+];
+
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
-        center: {lat: -34.397, lng: 150.644},
+        center: {lat: 28.538902, lng: -81.378981},
         zoom: 10
+    })
+
+    restaurants.forEach(res => {
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(res.lat, res.lng),
+            title: res.name
+        })
+
+        // To add marker to the map
+        marker.setMap(map);
     })
 }
 

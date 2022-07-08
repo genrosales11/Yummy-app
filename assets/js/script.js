bc82d107ff0625166ca5eb831c03d263fe348bb2
laurasierra17 <laura.sierra17@gmail.com>
2022-07-08 17:28:41 -0400

    commenting out map to stop requests

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,59 @@
-// Initialize Google Maps API
-var map;
-// Dummy data to display restaurants in a given area
-var restaurants = [
-    {
-        name: "Hawkers Asian Street Food",
-        lat: 28.560418,
-        lng: -81.364372
-    },
-    {
-        name: "Izziban Sushi & BBQ",
-        lat: 28.5535468,
-        lng: -81.3222981
-    }
-];
+// // Initialize Google Maps API
+// var map;
+// // Dummy data to display restaurants in a given area
+// var restaurants = [
+//     {
+//         name: "Hawkers Asian Street Food",
+//         lat: 28.560418,
+//         lng: -81.364372
+//     },
+//     {
+//         name: "Izziban Sushi & BBQ",
+//         lat: 28.5535468,
+//         lng: -81.3222981
+//     }
+// ];
 
-// Function that displays markers on the map for each restaurant
-function setMarkers(map) {
-    restaurants.forEach(res => {
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(res.lat, res.lng),
-            title: res.name
-        })
+// // Function that displays markers on the map for each restaurant
+// function setMarkers(map) {
+//     restaurants.forEach(res => {
+//         var marker = new google.maps.Marker({
+//             position: new google.maps.LatLng(res.lat, res.lng),
+//             title: res.name
+//         })
         
-        // To add marker to the map
-        marker.setMap(map);
+//         // To add marker to the map
+//         marker.setMap(map);
 
-        // Add an info window for each marker
-        var infoWindow = new google.maps.InfoWindow({
-            content: res.name
-        })
+//         // Add an info window for each marker
+//         var infoWindow = new google.maps.InfoWindow({
+//             content: res.name
+//         })
         
-        // Clicking at the marker displays the marker's associated restaurant name
-        marker.addListener("click", () => {
-            infoWindow.open({
-                anchor: marker,
-                map,
-                shouldFocus: false
-            })
-        })
-    })
-}
+//         // Clicking at the marker displays the marker's associated restaurant name
+//         marker.addListener("click", () => {
+//             infoWindow.open({
+//                 anchor: marker,
+//                 map,
+//                 shouldFocus: false
+//             })
+//         })
+//     })
+// }
 
-// Google Maps API function to display map
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: {lat: 28.538902, lng: -81.378981},
-        zoom: 12
-    })
+// // Google Maps API function to display map
+// function initMap() {
+//     map = new google.maps.Map(document.getElementById("map"), {
+//         center: {lat: 28.538902, lng: -81.378981},
+//         zoom: 12
+//     })
 
-    // Dummy data: for each restaurant in the list, show the marker on the map
-    setMarkers(map);
+//     // Dummy data: for each restaurant in the list, show the marker on the map
+//     setMarkers(map);
     
-}
+// }
 
-window.initMap = initMap;
+// window.initMap = initMap;
 
 let queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search";
 const apiKey = 'S9-0qQhgv6fiYrqXXE9X-TIZd1aepWF1BA6eT6_wQnQXtp8R1SVNuaXBGaZO15-IizPiprlRb6IT7v_gz_Dfl7yzGpZD_sCSnedvAD89GvwKGf85mwqLPWc5JGjIYnYx' //Add your key here

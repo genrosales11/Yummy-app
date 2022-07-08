95764c660ffb03bd91b324903528311242b499ba
laurasierra17 <laura.sierra17@gmail.com>
2022-07-08 14:06:47 -0400

    functionality to display markers on the map

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,12 +14,8 @@ var restaurants = [
     }
 ];
 
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: {lat: 28.538902, lng: -81.378981},
-        zoom: 10
-    })
-
+// Function that displays markers on the map for each restaurant
+function setMarkers(map) {
     restaurants.forEach(res => {
         var marker = new google.maps.Marker({
             position: new google.maps.LatLng(res.lat, res.lng),
@@ -29,6 +25,18 @@ function initMap() {
         // To add marker to the map
         marker.setMap(map);
     })
+}
+
+// Google Maps API function to display map
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {lat: 28.538902, lng: -81.378981},
+        zoom: 12
+    })
+
+    // Dummy data: for each restaurant in the list, show the marker on the map
+    setMarkers(map);
+    
 }
 
 window.initMap = initMap;

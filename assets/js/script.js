b3980ad48c6332f966f93433d1dbf42f0b901f37
Mary Dillon <mary.dillon09@gmail.com>
2022-07-09 12:43:55 -0700

    adds form dropdown and price points. connects to yelp api

--- assets/js/script.js
+++ assets/js/script.js
@@ -152,3 +152,10 @@ function addCat() {
 
 
 searchBtn.addEventListener("click", cityInput)
+
+// bulma dropdown event listener
+// var dropdown = document.querySelector('.dropdown');
+// dropdown.addEventListener('click', function(event) {
+//   event.stopPropagation();
+//   dropdown.classList.toggle('is-active');
+// });
